44f754e005dd6a020cf74d3e224e329bbc6f54a6
bburakbbasol <bburakbbasol@gmail.com>
2025-01-18 18:56:02 +0300

    Add project files.

---
+++ PratikList2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+//Kahveler listemizi oluştuduk
+List<string> Kahveler = new List<string>() { "Kahve1: Mocha", "Kahve2: Latte", "Kahve3: Maocha", "Kahve4: Mocha", "Kahve5: Cappucino" };
+//Kahveler listemizdeki kahveleri çektik
+foreach(string kahve in Kahveler)
+{
+	//Split metotu ile kahve1 vb. yazan kısımları kestik
+	Console.WriteLine(kahve.Split(':').Last());
+}
